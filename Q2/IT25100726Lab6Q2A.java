23da8d013be11e92dbac79ab549bc5134f214710
it25100726 <it25100726@my.sliit.lk>
2026-09-11 16:01:25 +0530

    Add files via upload
    IT25100726 Lab6Q2A , Q2B , Q2C

---
+++ Q2/IT25100726Lab6Q2A.java
@@ -0,0 +1,11 @@
+public class IT25100726Lab6Q2A {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
